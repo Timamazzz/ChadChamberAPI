3fa900ae845af1890d688e313b011b8091ebbe33
Артем <timforworking@mail.ru>
2024-05-25 23:38:57 +0300

    fix ServiceAdmin

--- services_app/admin.py
+++ services_app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import Service
+from django.db import models
+from ckeditor.widgets import CKEditorWidget
 
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'short_description')
     search_fields = ('name', 'short_description')
+    formfield_overrides = {
+        models.TextField: {'widget': CKEditorWidget}
+    }
